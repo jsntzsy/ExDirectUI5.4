caf8d954106035c670abec8c1c49b20227e4dcab
EternalZSY <910221550@qq.com>
2024-03-15 23:13:44 +0800

    增加memdc相关类型

--- ExDirectUI/include/drawing/render/mem_dc.h
+++ ExDirectUI/include/drawing/render/mem_dc.h
@@ -1,6 +1,6 @@
 ﻿/**
- * @file render.h
- * @brief 渲染器头文件
+ * @file mem_dc.h
+ * @brief 内存设备上下文头文件
  * @author EternalZSY
  * @version 5.4.0.0
  * @date 2024-03-14
@@ -8,24 +8,31 @@
  */
 #pragma once
 
-#include "render/image.h"
-#include "render/font.h"
-#include "render/matrix.h"
-#include "render/path.h"
-#include "render/region.h"
-#include "render/brush.h"
-#include "render/pen.h"
-#include "render/solid_brush.h"
-#include "render/linear_brush.h"
-#include "render/radial_brush.h"
-#include "render/image_brush.h"
-#include "render/canvas_brush.h"
-#include "render/effect.h"
-#include "render/device.h"
-#include "render/canvas.h"
-
 namespace ExDirectUI
 {
+	/// 内存设备上下文
+	struct ExMemDC
+	{
+		///宽度
+		uint32_t width;
 
+		///高度
+		uint32_t height;
+		
+		///设备上下文句柄
+		HDC dc;
+		
+		///位图句柄
+		HBITMAP bitmap;
+
+		///位图像素数据
+		EXBITSDATA* bits;
+	};
+
+	/////////////////////////////////
+
+	//HRESULT EXAPI EXCALL ExMemDCCreate(uint32_t width, uint32_t height, ExMemDC* r_dc);
+	//HRESULT EXAPI EXCALL ExMemDCDestroy(ExMemDC* dc);
+	//HRESULT EXAPI EXCALL ExMemDCResize(ExMemDC* dc, uint32_t new_width, uint32_t new_height);
 
 }
